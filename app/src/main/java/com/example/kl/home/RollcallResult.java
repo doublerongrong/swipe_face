aef14841e8bd88d6b216ab55393ac53fa711c927
kevin <kevin930135@gmail.com>
2019-05-09 20:07:36 +0800

    監聽返回鍵

--- app/src/main/java/com/example/kl/home/RollcallResult.java
+++ app/src/main/java/com/example/kl/home/RollcallResult.java
@@ -20,6 +20,7 @@ import android.support.design.widget.TabLayout;
 
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -530,4 +531,245 @@ public class RollcallResult extends AppCompatActivity implements ViewPager.OnPag
 
         });
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK )
+        {
+            DocumentReference docRef = db.collection("Rollcall").document(docId);
+            docRef.get().addOnSuccessListener(documentSnapshot -> {
+                lateList = (ArrayList) documentSnapshot.get("rollcall_late");
+                absenceList = (ArrayList) documentSnapshot.get("rollcall_absence");
+                attendList = (ArrayList)documentSnapshot.get("rollcall_attend");
+
+                Log.i("late",lateList.toString());
+                Log.i("absence",absenceList.toString());
+
+
+                if(!lateList.isEmpty()){
+                    for ( int j = 0; j < lateList.size(); j++) {
+                        latId = lateList.get(j);
+                        Query query = db.collection("Performance")
+                                .whereEqualTo("class_id", classId)
+                                .whereEqualTo("student_id", lateList.get(j));
+                        query.get().addOnCompleteListener(task -> {
+                            QuerySnapshot querySnapshot1 = task.isSuccessful() ? task.getResult() : null;
+                            Log.i("query", "work");
+                            for (DocumentSnapshot documentSnapshot2 : querySnapshot1.getDocuments()) {
+                                perf = documentSnapshot2.get("performance_totalAttendance").toString();
+                                absenceTimes = documentSnapshot2.get("performance_absenceTimes").toString();
+                                perId = documentSnapshot2.getId();
+                                score = Integer.parseInt(perf);
+                                abTimes = Integer.parseInt(absenceTimes);
+                                scoreList1.add(perf);
+                                absTimeList1.add(absenceTimes);
+                                if (oriAttend.contains(latId)){
+                                    score -= lateMinus;
+                                }
+                                if(oriLate.contains(latId)){
+                                    score = score;
+                                }
+                                if(oriAbsence.contains(latId)) {
+                                    score -= (lateMinus - absenteeMinus);
+                                    abTimes -= 1;
+                                }
+                                DocumentReference ChangePointRef = db.collection("Performance").document(perId);
+                                ChangePointRef.update("performance_totalAttendance", score);
+                                ChangePointRef.update("performance_absenceTimes",abTimes);
+                            }
+                            if (absTimeList1.size() == lateList.size()) {
+                                //寄缺席預警信
+                                for (int a = 0; a < absTimeList1.size(); a++) {
+                                    if (Integer.parseInt(absTimeList1.get(a)) >= ewatimes) {
+                                        Query query1 = db.collection("Student")
+                                                .whereEqualTo("student_id", lateList.get(a));
+                                        query1.get().addOnCompleteListener(task2 -> {
+                                            QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                            Log.i("query123", "work");
+                                            for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                lateEmail = documentSnapshot3.getString("student_email");
+                                                sendAbsenceWarningEmail(lateEmail, className);
+                                            }
+                                        });
+                                    }
+                                }
+                                for (int a = 0; a < scoreList1.size(); a++) {
+                                    //寄分數預警信
+                                    if (Integer.parseInt(scoreList1.get(a)) < ewpoint) {
+                                        Query query1 = db.collection("Student")
+                                                .whereEqualTo("student_id", lateList.get(a));
+                                        query1.get().addOnCompleteListener(task2 -> {
+                                            QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                            Log.i("query123", "work");
+                                            for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                lateEmail = documentSnapshot3.getString("student_email");
+                                                sendPointsWarningEmail(lateEmail, className);
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+
+                if(!absenceList.isEmpty()){
+                    for ( int k = 0; k < absenceList.size(); k++) {
+                        absId = absenceList.get(k);
+                        Log.i("absId",absId);
+                        Query query = db.collection("Performance")
+                                .whereEqualTo("class_id", classId)
+                                .whereEqualTo("student_id", absenceList.get(k));
+                        query.get().addOnCompleteListener(task -> {
+                            QuerySnapshot querySnapshot1 = task.isSuccessful() ? task.getResult() : null;
+                            Log.i("query123", "work");
+                            for (DocumentSnapshot documentSnapshot2 : querySnapshot1.getDocuments()) {
+                                perf = documentSnapshot2.get("performance_totalAttendance").toString();
+                                absenceTimes = documentSnapshot2.get("performance_absenceTimes").toString();
+                                Log.i("perf",perf);
+                                perId = documentSnapshot2.getId();
+                                score = Integer.parseInt(perf);
+                                abTimes = Integer.parseInt(absenceTimes);
+                                scoreList.add(perf);
+                                absTimeList.add(absenceTimes);
+                                if (oriAttend.contains(absId)){
+                                    Log.i("absence","work1");
+                                    score -= absenteeMinus;
+                                    abTimes += 1;
+                                }
+                                if (oriLate.contains(absId)){
+                                    Log.i("absence","work2");
+                                    score -= (absenteeMinus-lateMinus);
+                                    abTimes += 1;
+                                }
+                                if (oriAbsence.contains(absId)) {
+                                    Log.i("absence","work3");
+                                    score = score;
+                                }
+                                DocumentReference ChangePointRef = db.collection("Performance").document(perId);
+                                Map<String,Object> performance = new HashMap<>();
+                                performance.put("performance_totalAttendance", score);
+                                performance.put("performance_absenceTimes",abTimes);
+                                ChangePointRef.update(performance);
+                            }
+                            if (absTimeList.size() == absenceList.size()){
+                                //寄缺席預警信
+                                for (int a=0;a<absTimeList.size();a++){
+                                    if (Integer.parseInt(absTimeList.get(a)) >= ewatimes){
+                                        Log.i("stu_id",absenceList.get(a));
+                                        Query query1 = db.collection("Student")
+                                                .whereEqualTo("student_id",absenceList.get(a));
+                                        query1.get().addOnCompleteListener(task2 -> {
+                                            QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                            Log.i("query123", "work");
+                                            for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                absenceEmail = documentSnapshot3.getString("student_email");
+                                                sendAbsenceWarningEmail(absenceEmail,className);
+                                            }
+                                        });
+                                    }
+                                }
+                                for (int a=0;a<scoreList.size();a++){
+                                    //寄分數預警信
+                                    if (Integer.parseInt(scoreList.get(a)) < ewpoint){
+                                        Query query1 = db.collection("Student")
+                                                .whereEqualTo("student_id", absenceList.get(a));
+                                        query1.get().addOnCompleteListener(task2 -> {
+                                            QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                            Log.i("query123", "work");
+                                            for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                absenceEmail = documentSnapshot3.getString("student_email");
+                                                sendPointsWarningEmail(absenceEmail,className);
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+
+                        });
+                    }
+
+                }
+                if(!attendList.isEmpty()){
+                    for ( int l = 0; l < attendList.size(); l++) {
+                        attId = attendList.get(l);
+                        Query query = db.collection("Performance")
+                                .whereEqualTo("class_id", classId)
+                                .whereEqualTo("student_id", attendList.get(l));
+                        query.get().addOnCompleteListener(task -> {
+                            QuerySnapshot querySnapshot1 = task.isSuccessful() ? task.getResult() : null;
+                            Log.i("query", "work");
+                            for (DocumentSnapshot documentSnapshot2 : querySnapshot1.getDocuments()) {
+                                perf = documentSnapshot2.get("performance_totalAttendance").toString();
+                                absenceTimes = documentSnapshot2.get("performance_absenceTimes").toString();
+                                perId = documentSnapshot2.getId();
+                                score = Integer.parseInt(perf);
+                                abTimes = Integer.parseInt(absenceTimes);
+                                scoreList2.add(perf);
+                                absTimeList2.add(absenceTimes);
+                                if (oriAttend.contains(attId)){
+                                    score = score;
+                                }
+                                if (oriLate.contains(attId)){
+                                    score += lateMinus;
+                                }
+                                if (oriAbsence.contains(attId)) {
+                                    score += absenteeMinus;
+                                    abTimes -= 1;
+                                }
+                                DocumentReference ChangePointRef = db.collection("Performance").document(perId);
+                                ChangePointRef.update("performance_totalAttendance", score);
+                                ChangePointRef.update("performance_absenceTimes",abTimes);
+
+                                if (absTimeList2.size() == attendList.size()) {
+                                    //寄缺席預警信
+                                    for (int a = 0; a < absTimeList2.size(); a++) {
+                                        if (Integer.parseInt(absTimeList2.get(a)) >= ewatimes) {
+                                            Query query1 = db.collection("Student")
+                                                    .whereEqualTo("student_id", attendList.get(a));
+                                            query1.get().addOnCompleteListener(task2 -> {
+                                                QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                                Log.i("query123", "work");
+                                                for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                    attendEmail = documentSnapshot3.getString("student_email");
+                                                    sendAbsenceWarningEmail(attendEmail, className);
+                                                }
+                                            });
+                                        }
+                                    }
+                                    for (int a = 0; a < scoreList2.size(); a++) {
+                                        //寄分數預警信
+                                        if (Integer.parseInt(scoreList2.get(a)) < ewpoint) {
+                                            Query query1 = db.collection("Student")
+                                                    .whereEqualTo("student_id", attendList.get(a));
+                                            query1.get().addOnCompleteListener(task2 -> {
+                                                QuerySnapshot querySnapshot2 = task2.isSuccessful() ? task2.getResult() : null;
+                                                Log.i("query123", "work");
+                                                for (DocumentSnapshot documentSnapshot3 : querySnapshot2.getDocuments()) {
+                                                    attendEmail = documentSnapshot3.getString("student_email");
+                                                    sendPointsWarningEmail(attendEmail, className);
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), MainActivity.class);
+                intent.putExtra("class_id", classId);
+                intent.putExtra("classDoc_id", classDocId);
+                intent.putExtra("rollcall_id", docId);
+                intent.putExtra("request", 2);
+                startActivity(intent);
+                finish();
+            });
+
+        }
+        return false;
+    }
+
 }
