8a72e15526abf0110ae85b57857dcc807f5fd54b
yin1219 <kk8992008@gmail.com>
2019-05-16 21:33:17 +0800

    優化推撥(同帳號不同手機可以接收推撥) 20190516-yin

--- app/src/main/java/com/example/kl/home/SignUp.java
+++ app/src/main/java/com/example/kl/home/SignUp.java
@@ -43,8 +43,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db;
 
-    private List<String> class_id,teacher_officetime;
-    private String teacher_office,teacher_registrationToken;
+    private List<String> class_id,teacher_officetime,teacher_registrationToken;
+    private String teacher_office,str_teacher_registrationToken;
 
     private int check = 0, request;
 
@@ -251,7 +251,9 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                teacher_registrationToken = FirebaseInstanceId.getInstance().getToken();
+                                str_teacher_registrationToken = FirebaseInstanceId.getInstance().getToken();
+                                teacher_registrationToken = new ArrayList<>();
+                                teacher_registrationToken.add(str_teacher_registrationToken);
                                 Map<String, Object> user = new HashMap<>();
                                 user.put("class_id",class_id);
                                 user.put("teacher_email", email);
